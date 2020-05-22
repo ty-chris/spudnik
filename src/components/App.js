7494ff0381d147a50da7d63e0d062b5f1513e6c7
Chris Ng <tingyou@hotmail.co.uk>
2020-05-22 16:05:21 +0800

    init material ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import Button from "@material-ui/core/Button";
 
 class App extends React.Component {
-    render() {
-        return <div>TheMunchingChef</div>
-    }
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary">
+          TheMunchingChef
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
